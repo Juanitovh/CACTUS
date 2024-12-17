1ea33c9457c228c32cfa59d2b53cf2df71997117
juanluis-tw-os <jljnhp@gmail.com>
2024-12-17 11:49:28 +0100

    req file fixed

--- cactus_scripts/libraries/CactusPaths.py
+++ cactus_scripts/libraries/CactusPaths.py
@@ -3,8 +3,8 @@ from types import SimpleNamespace
 
 
 cactus_paths_dict = {
-    "cactus_code": str("/home/jppl/CACTUS/cactus_scripts"),
-    "global_optimizer": str("/home/jppl/CACTUS/cactus_scripts/joint_fibre_optimizer.out"),
+    "cactus_code": str("/home/juanluis/CACTUS/cactus_scripts"),
+    "global_optimizer": str("/home/juanluis/CACTUS/cactus_scripts/joint_fibre_optimizer.out"),
     "blender": str("/path/to/binary1"),
     "MCDC": str("/path/to/binary1"),
     "optimized_final":str("optimized_final.txt")
